91974cc3c5c90d488d83b365d6c7c14a584fb01f
Augustin <augustindelapena1904@gmail.com>
2023-05-10 22:38:49 +0800

    creating

--- 4Ps-financial-tracker/Main.cpp
+++ 4Ps-financial-tracker/Main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() {
 
 	cout << "Hello putangina 5 oras bago naayos" << endl;
-	cout << "second line" << endl;
+	cout << "second line, maya ko lagay 3rd" << endl;
 
 	system("PAUSE");
 
